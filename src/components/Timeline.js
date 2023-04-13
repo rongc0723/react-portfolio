fdbe61f8c236d4f6f71785cae295a6be23e40546
Rong Chen <rongc0723@gmail.com>
2023-04-13 12:30:35 -0400

    changes

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -10,21 +10,24 @@ const experiences = [
         company: "Amica Mutual Insurance",
         location: "(Remote)",
         position: "Student QA Developer",
-        icon: <FontAwesomeIcon icon={faBug} size="2x" />
+        icon: <FontAwesomeIcon icon={faBug} size="2x" />,
+        description: " Created a test automation framework using Selenium and Java to test the company's website. Developed a test suite that tests the company's website for bugs and errors."
     },
     {
         date: "Feb 2023 - Present",
         company: "Boston Hacks",
         location: "(Boston, MA)",
         position: "Software Engineer",
-        icon: <FontAwesomeIcon icon={faCode} size="2x" />
+        icon: <FontAwesomeIcon icon={faCode} size="2x" />,
+        description: "Redesigned and developed a new 2023 Boston Hacks hackathon website using React and Firebase. Created a website that hosts all previous Boston Hacks hackathons and their corresponding websites."
     },
     {
         date: "Jun 2022 - Aug 2022",
         company: "Youth4AM",
         location: "(Remote)",
         position: "Database Intern",
-        icon: <FontAwesomeIcon icon={faDatabase} size="2x" />
+        icon: <FontAwesomeIcon icon={faDatabase} size="2x" />,
+        description: "Created a MySQL database that stores all company data and allows for easy access to data from scratch. Designed a user-friendly interface that allowed 100+ teaching assistnts to easily access and update student data using Javacript and PHP."
     }
 ]
 
@@ -40,8 +43,8 @@ const Timeline = () => {
                     dateClassName="date"
                     icon={experience.icon}
                 >
-                    <h3 className="vertical-timeline-element-title">{experience.company}</h3>
-                    <h4 className="vertical-timeline-element-subtitle">{experience.position}</h4>
+                    <h3 className="vertical-timeline-element-title">{experience.company + " - " + experience.position}</h3>
+                    <p>{experience.description}</p>
                 </VerticalTimelineElement>
             ))}
         </VerticalTimeline>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,22 @@
-import React from 'react'
-
+import './Contact.css'
 export default function Contact() {
+
   return (
     <div className='contact'>
-      <h1>Contact</h1>
-      <p>lorem is</p>
+        <div className='map'>
+        <iframe title="hojo" width="100%" height="300" frameborder="0" src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=575%20Commonwealth%20Ave+(Dorm)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">Hojo</a>
+        </iframe>
+        </div>
+        <div className='contact-info'>
+          <h1>Contact Me</h1>
+          <ul>
+            <li><a href='https://www.linkedin.com/in/rong-chen-421b9b219/' target='blank'>🔗 Linkedin </a></li>
+            <li><a href='https://github.com/rongc0723' target='blank'>🔗 Github </a></li>
+            <li><a href='https://www.instagram.com/derpy_rong/' target='blank'>🔗 Instagram </a></li>
+            <li><a href='mailto:rongc@bu.edu'>📧 rongc@bu.edu</a></li>
+            <li>📍 575 Commonwealth Avenue<br/>Boston, MA 02215</li>
+          </ul>
+        </div>
     </div>
   )
 }
